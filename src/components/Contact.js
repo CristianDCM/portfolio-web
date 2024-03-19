10ece31f6e40d6c4b90a485a6f76c6fc8bb20f92
CristianDCM <cristianzdmc14@gmail.com>
2024-03-19 15:21:05 -0500

    Actualizar formulario de contacto

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,13 +9,14 @@ export const Contact = () => {
           <Col size={12} md={6}>
             <div className="animate__animated animate__fadeIn">
               <h2 data-aos="fade-right">Contact</h2>
-              <form name="contact" method="POST" netlify> 
+              <form name="contact" method="POST" data-netlify="true" onSubmit="submit"> 
                 <Row>
+                  <input type="hidden" name="form-name" value="contact" />
                   <Col size={12} sm={6} className="px-1">
-                    <input type="text" name="firstname" placeholder="First Name" data-aos="zoom-in-right" required />
+                    <input type="text" name="first-name" placeholder="First Name" data-aos="zoom-in-right" required />
                   </Col>
                   <Col size={12} sm={6} className="px-1">
-                    <input type="text" name="lastname" placeholder="Last Name" data-aos="zoom-in-right" required />
+                    <input type="text" name="last-name" placeholder="Last Name" data-aos="zoom-in-right" required />
                   </Col>
                   <Col size={12} sm={6} className="px-1">
                     <input type="email" name="email" placeholder="Email Address" data-aos="zoom-in-left" required />
