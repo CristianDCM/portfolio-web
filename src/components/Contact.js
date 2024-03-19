562e9318449a3daab9636e8ec2e3f127d49fc65a
CristianDCM <cristianzdmc14@gmail.com>
2024-03-19 17:41:19 -0500

    Actualizar formulario de contacto en Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ export const Contact = () => {
           <Col size={12} md={6}>
             <div className="animate__animated animate__fadeIn">
               <h2 data-aos="fade-right">Contact</h2>
-              <form name="contact" method="post" netlify>
+              <form name="contact" netlify>
                 <Row>
                   <Col size={12} sm={6} className="px-1">
                     <input type="hidden" name="form-name" value="contact"/>
