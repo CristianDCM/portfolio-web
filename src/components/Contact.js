1584ad9422a5681ee690dad6c2b5ac948f882eb0
CristianDCM <cristianzdmc14@gmail.com>
2024-03-19 18:01:01 -0500

    Añadidos campos de nombre, correo electrónico y mensaje en el formulario de contacto

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,10 @@ import { Container, Row, Col } from "react-bootstrap";
 import 'animate.css';
 
 export const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
   return (
     <section className="contact" id="contact"> 
       <Container>
@@ -9,22 +13,23 @@ export const Contact = () => {
           <Col size={12} md={6}>
             <div className="animate__animated animate__fadeIn">
               <h2 data-aos="fade-right">Contact</h2>
-              <form name="contact" id="contact" data-netlify="true" data-netlify-honeypot="bot-field">
+              <form name="contact" data-netlify="true" method="POST">
+                <input type="hidden" name="form-name" value="contact" />
                 <Row>
                   <Col size={12} sm={6} className="px-1">
-                    <input type="text" name="name" placeholder="First Name" data-aos="zoom-in-right" required/>
+                    <input type="text" name="first-name" placeholder="First Name" data-aos="zoom-in-right" value={name} onChange={(e) => setName(e.target.value)} required/>
                   </Col>
                   <Col size={12} sm={6} className="px-1">
-                    <input type="text" name="name" placeholder="Last Name" data-aos="zoom-in-right" required/>
+                    <input type="text" name="last-name" placeholder="Last Name" data-aos="zoom-in-right" value={name} onChange={(e) => setName(e.target.value)} required/>
                   </Col>
                   <Col size={12} sm={6} className="px-1">
-                    <input type="email" name="email" placeholder="Email Address" data-aos="zoom-in-left" required/>
+                    <input type="email" name="email" placeholder="Email Address" data-aos="zoom-in-left" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                   </Col>
                   <Col size={12} sm={6} className="px-1">
-                    <input type="tel" name="tel" placeholder="Phone" data-aos="zoom-in-left" required/>
+                    <input type="tel" name="tel" placeholder="Phone" data-aos="zoom-in-left" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                   </Col>
                   <Col size={12} className="px-1" date-aos="fade-up">
-                    <textarea rows="6" name="message" placeholder="Message" data-aos="zoom-in" required></textarea>
+                    <textarea rows="6" name="message" placeholder="Message" data-aos="zoom-in" value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
                     <button type="submit"><span>Send</span></button>
                   </Col>
                 </Row>
