645ac5addb1999e2f15436f3a79fcb487f93e356
CristianDCM <cristianzdmc14@gmail.com>
2024-03-20 14:50:20 -0500

    Corregir errores de escritura en Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -49,13 +49,13 @@ export const Contact = () => {
                     <input type="text" name="first-name" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                   </Col>
                   <Col size={12} sm={6} className="px-1">
-                    <input type="text" name="last-name" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                    <input type="text" name="last-name" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                   </Col>
                   <Col size={12} sm={6} className="px-1">
                     <input type="email" name="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
                   </Col>
                   <Col size={12} sm={6} className="px-1">
-                    <input type="tel" name="phone" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                    <input type="tel" name="phone" value={formDetails.tel} placeholder="Phone No." onChange={(e) => onFormUpdate('tel', e.target.value)}/>
                   </Col>
                   <Col size={12} className="px-1" date-aos="fade-up">
                     <textarea rows="6" name="message" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
