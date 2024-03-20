c9b878566f7d5438d4dc2b8790de679ce2db620e
CristianDCM <cristianzdmc14@gmail.com>
2024-03-20 15:48:09 -0500

    Corregir formato de fetch en Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ export const Contact = () => {
     e.preventDefault();
     const myForm = e.target;
     const formData = new FormData(myForm);
-    fetch("/",{ 
+    fetch("/",{
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
