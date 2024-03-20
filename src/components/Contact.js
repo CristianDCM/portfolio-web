6d746bf369a0f6a01ea9baa3a2e319f48f42f83b
CristianDCM <cristianzdmc14@gmail.com>
2024-03-20 15:23:41 -0500

    Corregir error en el envío del formulario

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,7 +21,7 @@ export const Contact = () => {
     const myForm = e.target;
     const formData = new FormData(myForm);
     setButtonText("Sending...");
-    let response = await fetch("/",{
+    let response = fetch("/",{
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
