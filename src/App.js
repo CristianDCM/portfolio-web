817bfe1aadf8e9f5ebb895c18fec708a835660d6
CristianDCM <cristianzdmc14@gmail.com>
2024-03-22 23:52:14 -0500

    Eliminar llamada a la API innecesaria en App.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,12 @@ import { AboutMe } from "./components/AboutMe";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import React, { useEffect } from 'react';
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init({
   duration: 500,
 });
 function App() {
-  useEffect(() => {
-    fetch("/api")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-  },[])
   return (
     <div className="App">
       <NavBar />
