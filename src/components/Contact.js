a6ac83b58055e5891e03cf06b951f936a873f36e
CristianDCM <cristianzdmc14@gmail.com>
2024-03-23 00:09:04 -0500

    Actualizar formulario de contacto

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,8 +9,8 @@ export const Contact = () => {
           <Col size={12} md={6}>
             <div className="animate__animated animate__fadeIn">
               <h2 data-aos="fade-right">Contact</h2>
-              <form name="contact-ntl">
-                <input type="hidden" name="form-name" value="contact-ntl" method="post"/>
+              <form name="contact-ntl" method="post">
+                <input type="hidden" name="form-name" value="contact-ntl"/>
               <Row>
                   <Col size={12} sm={6} className="px-1">
                     <input type="text" name="firstName" placeholder="First Name"/>
